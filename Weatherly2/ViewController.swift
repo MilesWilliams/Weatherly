6e3c6b66079cf55b8298f152989e56b2c6da0999
MilesWilliams <miles@klarity.co.za>
2017-04-29 12:39:42 +0200

    Initial Commit

---
+++ Weatherly2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weatherly2
+//
+//  Created by MIles Work on 2017/04/29.
+//  Copyright © 2017 MIles Work. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
